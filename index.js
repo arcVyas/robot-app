24b742a75efe94ab5465da9aa3639b630481c8c1
Vyas <vyas.mohan@gmail.com>
2017-08-06 18:10:12 -0500

    port fix

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.set('view engine', 'pug');
 var pieRouter = require('./routes/pie')
 app.use('/pie',pieRouter)
 
-app.set('port', 9080);
-var server = app.listen(app.get('port'), function() {
+//app.set('port', 9080);
+var server = app.listen(process.env.PORT || 9080, function() {
   console.log('Express server listening on port ' + server.address().port);
 });
 module.exports = app;
